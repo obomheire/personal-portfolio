33ee0520fa5726b8c1c786e8e1fba4552db11de2
Zack Bello <obomheire@gmail.com>
2022-09-24 17:46:26 +0100

    Adde additional pic

--- src/components/Projects/projectImages.js
+++ src/components/Projects/projectImages.js
@@ -2,8 +2,8 @@ import project1Laptop from "../../assets/projects/project-1-laptop.jpg";
 import project1Mobile from "../../assets/projects/project-1-mobile.jpg";
 import project2Laptop from "../../assets/projects/project-2-laptop.jpg";
 import project2Mobile from "../../assets/projects/project-2-mobile.jpg";
-import project3Laptop from "../../assets/projects/project-3-laptop.jpg";
-import project3Mobile from "../../assets/projects/project-3-mobile.jpg";
+import project3Laptop from "../../assets/projects/project-3-laptop.png";
+import project3Mobile from "../../assets/projects/project-3-mobile.png";
 import project4Laptop from "../../assets/projects/project-4-laptop.jpg";
 import project4Mobile from "../../assets/projects/project-4-mobile.jpg";
 
